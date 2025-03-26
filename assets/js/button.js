82ebc604fae411fb742124c9b6503f48deb695a8
sabacata <sabiankmcatalano@gmail.com>
2025-03-26 13:24:13 +0100

    Add files via upload

--- assets/js/button.js
+++ assets/js/button.js
@@ -12,3 +12,13 @@ function helpbutton() {
 	document.getElementById("helptext").innerHTML = "HELPPPP";
 	
 }
+
+function sabian3Filter() {
+                let tableau = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+                resultat = tableau.filter(element => element % 2 == 0);
+    
+                document.getElementById("sabian3filterCodeHolder").innerHTML = `let tableau = ${'[' + tableau.join(', ') + ']'}
+resultat = tableau.filter(element => element % 2 == 0);`;
+
+                document.getElementById("sabian3filterHolder").innerHTML = `resultat vaut ${'[' + resultat.join(', ') + ']'}`;
+            }
